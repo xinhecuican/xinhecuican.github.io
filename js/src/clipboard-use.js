8bd8b039593a8cb3da4b8d94b07b113a96d4a719
xinhecuican <lizilin@tju.edu.cn>
2020-04-06 19:15:12 +0800

    Site updated: 2020-04-06 19:14:46

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,17 +1,3 @@
-/*页面载入完成后，创建复制按钮*/
-!function (e, t, a) { 
-  /* code */
-  var initCopyCode = function(){
-    var copyHtml = '';
-    copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
-    copyHtml += '  <i class="fa fa-globe"></i><span>copy</span>';
-    copyHtml += '</button>';
-    $(".highlight .code pre").before(copyHtml);
-    new ClipboardJS('.btn-copy', {
-        target: function(trigger) {
-            return trigger.nextElementSibling;
-        }
-    });
-  }
-  initCopyCode();
-}(window, document);
+// build time:Mon Apr 06 2020 19:14:40 GMT+0800 (中国标准时间)
+!function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-globe"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
+//rebuild by neat 
