3ef5dcfa8270642005c6042b451ec514751989af
xinhecuican <lizilin@tju.edu.cn>
2020-04-09 14:26:15 +0800

    Site updated: 2020-04-09 14:25:59

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Thu Apr 09 2020 14:21:52 GMT+0800 (中国标准时间)
+// build time:Thu Apr 09 2020 14:25:54 GMT+0800 (中国标准时间)
 !function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-globe"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
 //rebuild by neat 
