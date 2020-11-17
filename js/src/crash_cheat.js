b9d45bdc5676645e8063d5cbc216ef14132f10cf
xinhecuican <lizilin@tju.edu.cn>
2020-11-17 21:43:20 +0800

    Site updated: 2020-11-17 21:43:01

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,20 @@
-// build time:Tue Nov 17 2020 20:57:38 GMT+0800 (中国标准时间)
-$(window).load(function(){var e=document.title;var t;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","../../images/failure.png");$('[rel="shortcut icon"]').attr("href","../../images/failure.png");document.title="(つェ⊂) 我藏好了哦~ ";clearTimeout(t)}else{$('[rel="icon"]').attr("href","../../images/favicon.png");$('[rel="shortcut icon"]').attr("href","../../images/favicon.png");document.title="o(^▽^)o 被你发现啦~ ";t=setTimeout(function(){document.title=e},2e3)}})});
-//rebuild by neat 
+$(window).load(function () {
+//整合页面欺骗特效 window.onload有冲突
+    var OriginTitile = document.title;
+    var titleTime;
+    document.addEventListener('visibilitychange', function () {
+        if (document.hidden) {
+            $('[rel="icon"]').attr('href', "../../images/failure.png");
+            $('[rel="shortcut icon"]').attr('href', "../../images/failure.png");
+            document.title = '(つェ⊂) 我藏好了哦~ ';
+            clearTimeout(titleTime);
+        } else {
+            $('[rel="icon"]').attr('href', "../../images/favicon.png");
+            $('[rel="shortcut icon"]').attr('href', "../../images/favicon.png");
+            document.title = 'o(^▽^)o 被你发现啦~ ';
+            titleTime = setTimeout(function () {
+                document.title = OriginTitile;
+            }, 2000);
+        }
+    });
+});
