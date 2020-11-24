10b01d149bc6d55b128c7e79be5de6a318d2fdb3
xinhecuican <lizilin@tju.edu.cn>
2020-11-24 19:24:00 +0800

    Site updated: 2020-11-24 19:23:48

---
+++ js/mychange.js
@@ -0,0 +1,8 @@
+var xiaokang = new xkTool("transparent");
+xiaokang.randomBanner(
+    "https://cdn.jsdelivr.net/gh/yunwanjia-cloud/banner/", // 前半部分网址
+    "-min.jpg", // 后半部分网址
+    0, // 随机数开始范围
+    2, // 随机数结束范围
+    true // 是否开启滤镜 默认不开启
+);
