e24a682ec58b6cc3772c9ddd1d4f725736039a19
xinhecuican <lizilin@tju.edu.cn>
2020-12-16 20:54:45 +0800

    Site updated: 2020-12-16 20:54:35

--- js/mychange.js
+++ js/mychange.js
@@ -1,1 +1,12 @@
-var xiaokang = new xkTool("transparent");
+var name = "xinhecuican";
+var repository = "xinhecuican.github.io";
+if(document.domain != 'localhost'){
+	if(document.getElementById("post")){
+		for (var key in document.getElementById("article-container").getElementsByTagName('img')){
+  			if (isNaN(key)){
+    			break;
+  			}
+    		document.getElementById("article-container").getElementsByTagName('img')[key].src = document.getElementById("article-container").getElementsByTagName('img')[key].src.replace(document.domain,"cdn.jsdelivr.net/gh/" + name + "/" + repository)
+		}
+    }
+}
