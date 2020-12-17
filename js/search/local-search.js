8a181763e2106d95e41e6677eb9d9c7d4d2e734c
xinhecuican <lizilin@tju.edu.cn>
2020-12-17 09:00:17 +0800

    Site updated: 2020-12-17 08:59:44

--- js/search/local-search.js
+++ js/search/local-search.js
@@ -42,7 +42,7 @@ window.addEventListener('load', () => {
   })
 
   function search (path) {
-    fetch(GLOBAL_CONFIG.root + path)
+    fetch("https://cdn.jsdelivr.net/gh/xinhecuican/xinhecuican.github.io/search.xml")
       .then(response => response.text())
       .then(str => new window.DOMParser().parseFromString(str, 'text/xml'))
       .then(data => {
@@ -71,8 +71,8 @@ window.addEventListener('load', () => {
             }
             let dataTitle = data.title.trim().toLowerCase()
             const dataContent = data.content.trim().replace(/<[^>]+>/g, '').toLowerCase()
-            //const dataUrl = data.url.startsWith('/') ? data.url : GLOBAL_CONFIG.root + data.url
-            const dataUrl = "https://cdn.jsdelivr.net/gh/xinhecuican/xinhecuican.github.io/search.xml"
+            const dataUrl = data.url.startsWith('/') ? data.url : GLOBAL_CONFIG.root + data.url
+            //const dataUrl = "https://cdn.jsdelivr.net/gh/xinhecuican/xinhecuican.github.io/search.xml"
             let indexTitle = -1
             let indexContent = -1
             let firstOccur = -1
