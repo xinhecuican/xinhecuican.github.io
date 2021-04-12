b83960b80f2246c63a9f533a779594a13fac9bea
xinhecuican <lizilin@tju.edu.cn>
2021-04-12 19:06:37 +0800

    Site updated: 2021-04-12 19:06:19

--- js/mychange.js
+++ js/mychange.js
@@ -1,9 +1,13 @@
 var name = "xinhecuican";
 var repository = "xinhecuican.github.io";
-if(document.domain != 'localhost'){
-	if(document.getElementById("post")){
-		for (var key in document.getElementById("article-container").getElementsByTagName('img')){
-  			if (isNaN(key)){
+if(document.domain != 'localhost')
+{
+	if(document.getElementById("post"))
+	{
+		for (var key in document.getElementById("article-container").getElementsByTagName('img'))
+		{
+  			if (isNaN(key))
+			{
     			break;
   			}
     		document.getElementById("article-container").getElementsByTagName('img')[key].src = document.getElementById("article-container").getElementsByTagName('img')[key].src.replace(document.domain,"cdn.jsdelivr.net/gh/" + name + "/" + repository)
