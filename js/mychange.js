a7da1d246b57ddd92c66abde39a47210ff255076
xinhecuican <lizilin@tju.edu.cn>
2021-04-26 19:23:18 +0800

    Site updated: 2021-04-26 19:23:09

--- js/mychange.js
+++ js/mychange.js
@@ -1,10 +1,11 @@
 var name = "xinhecuican";
 var repository = "xinhecuican.github.io";
+var total_picture = 8980
 if(document.domain != 'localhost')
 {
 	if(document.getElementById("post"))
 	{
-		var banner_index1 = Math.ceil(Math.random() * 3518);
+		var banner_index1 = Math.ceil(Math.random() * total_picture);
 		document.getElementById("page-header").style.backgroundImage = "url(https://cdn.jsdelivr.net/gh/xinhecuican/Resources/img/pic_" + banner_index1 + ".jpg)";
 		for (var key in document.getElementById("article-container").getElementsByTagName('img'))
 		{
@@ -27,7 +28,7 @@ if(document.getElementById("recent-posts"))
 		{
 			break;
 		}
-		var banner_index = Math.ceil(Math.random() * 3518)
+		var banner_index = Math.ceil(Math.random() * total_picture)
 		document.getElementById("recent-posts").getElementsByTagName('img')[key].src = "https://cdn.jsdelivr.net/gh/xinhecuican/Resources/img/pic_" + banner_index + ".jpg";
 	}
 }
