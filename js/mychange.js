9f7b7cc56eccf139fe693d7b4f2313b8446bc076
xinhecuican <lizilin@tju.edu.cn>
2021-04-30 17:25:10 +0800

    Site updated: 2021-04-30 17:24:54

--- js/mychange.js
+++ js/mychange.js
@@ -1,6 +1,6 @@
 var name = "xinhecuican";
 var repository = "xinhecuican.github.io";
-var total_picture = 10487
+var total_picture = 12211
 if(document.domain != 'localhost')
 {
 	if(document.getElementById("post"))
